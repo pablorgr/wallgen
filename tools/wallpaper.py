6fdf3d3858a4cd8c98cbcb8904d3f4d80f9d549f
Pablo Riutort <pablo.riutort@gmail.com>
2021-02-16 22:48:01 +0100

    fix ValueError when desktop not found
    Expecting 2 values

--- tools/wallpaper.py
+++ tools/wallpaper.py
@@ -80,11 +80,9 @@ def setwallpaper(image_path, relative_path=True):
                     + image_path + "')"])
         else:
             if desktop:
-                return f"Sorry, {desktop} is currently not supported.",
-                False
+                return f"Sorry, {desktop} is currently not supported.", False
             else:
-                return "Sorry, Desktop Environment could not be detected",
-                False
+                return "Sorry, Desktop Environment could not be detected", False
     elif "darwin" in host:
         db_file = "Library/Application Support/Dock/desktoppicture.db"
         db_path = os.path.join(
